372943c1aba536a057534078c6c8dbd8144fc5cf
SBester001 <SBester001@t-online.de>
2020-04-05 22:41:44 +0200

    added gyro
    not working properly

--- Nano_WLAN/Nano_WLAN.ino
+++ Nano_WLAN/Nano_WLAN.ino
@@ -1,44 +1,78 @@
+#include <Adafruit_MPU6050.h>
+#include <Wire.h>
+#include <math.h>
 #include <SoftwareSerial.h>
 
-String getOutput();
+String getWLAN();
+void getGyro();
+void calibrateGyro();
+void printGyro();
 
 SoftwareSerial ESP8266(11, 10);  //RX,TX
+const int MPU=0x68; 
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+int AcXOff, AcYOff, AcZOff, TmpOff, GyXOff, GyYOff, GyZOff;
+
+int i = 0;
 
 void setup()
 {
   Serial.begin(9600);
+  
+  //Setup WLAN
   ESP8266.begin(115200);
   while(!ESP8266);
   ESP8266.println("AT+UART_CUR=9600,8,1,0,0");
-  getOutput();
+  delay(100);
+  getWLAN();
   
   ESP8266.begin(9600);
   while(!ESP8266);
   ESP8266.println("AT+GMR"); //checks version information
-  getOutput();
+  delay(100);
+  getWLAN();
   
   ESP8266.println("ATE1"); //switches echo on
-  getOutput();
+  delay(100);
+  getWLAN();
   
   ESP8266.println("AT+CWMODE=3"); //set SoftAP + Station mode
-  getOutput();
-
+  delay(100);
+  getWLAN();
+  
   ESP8266.println("AT+CIPMUX=1"); //max 1 connection
-  getOutput();
-
+  delay(100);
+  getWLAN();
+  
   ESP8266.println("AT+CIPSERVER=1,100"); //start tcp server, port
-  getOutput();
-
+  delay(100);
+  getWLAN();
+  
   ESP8266.println("AT+CWSAP_CUR=\"drone\",\"1234567890\",5,3"); //set ssid, password, channel, encryption (2: WPA2_PSK)
-  getOutput();
+  delay(100);
+  getWLAN();
+  
+  //Setup Gyro
+  Wire.begin(); //Needs SCL -> A5  SDA -> A4
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  calibrateGyro();
 }
 
 void loop()
 {
-  getOutput();
+  ++i;
+  getWLAN();
+  printGyro();
+  if (i % 10 == 0) {
+    calibrateGyro();
+  }
+  delay(1000);
 }
 
-String getOutput()
+String getWLAN()
 {
   String output = "";
   delay(100);
@@ -53,4 +87,48 @@ String getOutput()
     Serial.println(output);
   }
   return output;
+}
+
+void getGyro()
+{
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true);
+  AcX = (Wire.read() << 8 | Wire.read()) - AcXOff;
+  AcY = (Wire.read() << 8 | Wire.read()) - AcYOff;
+  AcZ = (Wire.read() << 8 | Wire.read()) - AcZOff;
+  Tmp = (Wire.read() << 8 | Wire.read()) - TmpOff;
+  GyX = (Wire.read() << 8 | Wire.read()) - GyXOff;
+  GyY = (Wire.read() << 8 | Wire.read()) - GyYOff;
+  GyZ = (Wire.read() << 8 | Wire.read()) - GyZOff;
+
+  Serial.println();
+}
+
+void calibrateGyro()
+{
+  getGyro();
+  AcXOff += AcX;
+  AcYOff += AcY;
+  AcZOff += AcZ;
+  TmpOff += Tmp;
+  GyXOff += GyX;
+  GyYOff += GyY;
+  GyZOff += GyZ;
+
+  Serial.println("Calibrated Gyro");
+}
+
+void printGyro()
+{
+  getGyro();
+  
+  Serial.print("AcX: "); Serial.println(AcX);
+  Serial.print("AcY: "); Serial.println(AcY);
+  Serial.print("AcZ: "); Serial.println(AcZ);
+  Serial.print("Tmp: "); Serial.println(Tmp);
+  Serial.print("GyX: "); Serial.println(GyX);
+  Serial.print("GyY: "); Serial.println(GyY);
+  Serial.print("GyZ: "); Serial.println(GyZ);
 }
