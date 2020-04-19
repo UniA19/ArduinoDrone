d977f02aece7cec92b02de4b432bb712725fc141
SBester001 <SBester001@t-online.de>
2020-04-20 00:39:39 +0200

    Update FlightControl.ino
    changed for testing

--- FlightControl/FlightControl.ino
+++ FlightControl/FlightControl.ino
@@ -39,16 +39,18 @@
 
 
 #define WLAN_SSID "Drone"         //Name of the drone visible for devices
-#define WLAN_PASSWORD "dronepw"   //WLAN Password
+#define WLAN_PASSWORD "dronepword"   //WLAN Password 8 ~ 64 bytes ASCII
 #define WLAN_CHANNEL "5"          //WLAN Channel
 #define WLAN_TIMEOUT "2500"       //time in microseconds with no transmission that is still considered an active connection TODO: write function that lands the drone after lost connection
 
 #include <I2Cdev.h>                           //Library for the MPU-6050 (Gyro and Accel)
 #include <MPU6050_6Axis_MotionApps_V6_12.h>   //Firmware for the built in Processor of the Gyroscope (D igital M otion P rocessor) (DMP)
 #include <Wire.h>                             //Arduino Library for IC2 Protocol (in this case to talk to the MPU-6050 on Pins A4(SDA) and A5(SCL))
+#include <SoftwareSerial.h>
 
 
 MPU6050 mpu;
+SoftwareSerial ESP8266(11, 10);  //RX,TX
 
 //gloabl variables for...
 //keeping track of the last transmission via WLAN so the drone can shut off after set time when connection is lost
@@ -82,30 +84,41 @@ float desiredYawSpeed;                         //Angular Yaw speed in Rad/s
 //standard routine:
 void setup()
 {
+  Serial.begin(9600);
+  while(!Serial);
   startupWireless();
-  startupDMP();
+  //startupDMP();
   timeOfLastTransmission = micros();
 }
 
 void loop()     //pushes the drone towards the desired state from the actual state, those two are updated through interrupts
 {
-
+  //Serial.println("Hallo");
+  if (ESP8266.available()) {
+    SerialRead();
+    Serial.println(desiredVelocityX);
+  Serial.println(desiredVelocityY);
+  Serial.println(desiredVelocityZ);
+  Serial.println(desiredYawSpeed);
+  Serial.println("----------------------");
+  }
+  delay(100);
 }
 
 
 
 //interrupt functions:
-void SerialEvent()                //Refreshes the global Variables that keep track of the desired drone state and updates the timer of when the last transmission took place.
+void SerialRead()                //Refreshes the global Variables that keep track of the desired drone state and updates the timer of when the last transmission took place.
 {
-  if (Serial.read() != '<') return;      //checks if its a desired input from us or junk from the WLAN-module
+  if (ESP8266.read() != '<') return;      //checks if its a desired input from us or junk from the WLAN-module
   timeOfLastTransmission = micros();
-  if (Serial.read() == '#') {                         //checks if its a command
+  if (ESP8266.read() == '#') {                         //checks if its a command
     desiredYawSpeed = parseSerialPercentage() * (MAX_ANGLE_VELOCITY_YAW / 100);
-    Serial.read(); //remove the seperator "|"
+    ESP8266.read(); //remove the seperator "|"
     desiredVelocityZ = parseSerialPercentage() * (MAX_VELOCITY_Z / 100.0);
-    Serial.read(); //remove the seperator "|"
+    ESP8266.read(); //remove the seperator "|"
     desiredVelocityX = parseSerialPercentage() * (MAX_VELOCITY_X / 100.0);
-    Serial.read(); //remove the seperator "|"
+    ESP8266.read(); //remove the seperator "|"
     desiredVelocityY = parseSerialPercentage() * (MAX_VELOCITY_Y / 100.0);
   } else {
     
@@ -128,19 +141,22 @@ void startupDMP()
   Wire.setClock(400000);
   mpu.initialize();
 
-  mpu.dmpInitialize();                  //loads firmware for the DMP if it seems to fail it can be checked through return value but for now I assume it wont fail. 0 means success
+  Serial.println("init: " + String(mpu.dmpInitialize()));                  //loads firmware for the DMP if it seems to fail it can be checked through return value but for now I assume it wont fail. 0 means success
 
   mpu.setXGyroOffset(GYRO_OFFSET_X);    //set initial Offsets see comment at the constants
   mpu.setYGyroOffset(GYRO_OFFSET_Y);
   mpu.setZGyroOffset(GYRO_OFFSET_Z);
+  
   mpu.setXAccelOffset(ACCEL_OFFSET_X);
   mpu.setYAccelOffset(ACCEL_OFFSET_Y);
   mpu.setZAccelOffset(ACCEL_OFFSET_Z);
-
+Serial.println("start0");
   mpu.CalibrateAccel(6);                //TODO: find out what these two 6es do and demistify them
+  Serial.println("start1");
   mpu.CalibrateGyro(6);
-  //mpu.printActiveOffsets();           //I leave this here for later to shed some light on the offset magic
-
+  //mpu.PrintActiveOffsets();           //I leave this here for later to shed some light on the offset magic
+  delay(1000);
+Serial.println("start2");
   mpu.setDMPEnabled(true);
 
   pinMode(INTERRUPT_PIN, INPUT);        //sets the Pin INTERRUPT_PIN to INPUT mode
@@ -153,34 +169,42 @@ void startupDMP()
 
 void startupWireless()
 {
-  Serial.begin(115200); //The WLAN module is connected via Serial
-  Serial.println("AT+UART_CUR=9600,8,1,0,0"); //Tell it to Slow down
-  Serial.begin(9600);   //Slow down yourself
-  Serial.println("ATE0"); //switches echo (1: on 0: off)
-  Serial.println("AT+CWMODE=3"); //set SoftAP + Station mode
-  Serial.println("AT+CIPMUX=1");  //multiple connections needed for tcp server!!
-  Serial.println("AT+CIPSERVER=1,100"); //start TCP server using port 100
-  Serial.println("AT+CWSAP_CUR=\"" + (String) WLAN_SSID + "\",\"" + (String) WLAN_PASSWORD + "\"," + (String) WLAN_CHANNEL + ",3,1"); //setting SSID, Password, Channel, Encryption (3=WPA2_PSK), Only one user
+  ESP8266.begin(115200); //The WLAN module is connected via Serial
+  while(!ESP8266);
+  ESP8266.println("AT+UART_CUR=9600,8,1,0,0"); //Tell it to Slow down
+  delay(100);
+  ESP8266.begin(9600);   //Slow down yourself
+  ESP8266.println("ATE0"); //switches echo (1: on 0: off)
+  delay(100);
+  ESP8266.println("AT+CWMODE=3"); //set SoftAP + Station mode
+  delay(100);
+  ESP8266.println("AT+CIPMUX=1");  //multiple connections needed for tcp server!!
+  delay(100);
+  ESP8266.println("AT+CIPSERVER=1,100"); //start TCP server using port 100
+  delay(100);
+  Serial.println("AT+CWSAP_CUR=\"" + (String) WLAN_SSID + "\",\"" + (String) WLAN_PASSWORD + "\"," + (String) WLAN_CHANNEL + ",3,1");
+  ESP8266.println("AT+CWSAP_CUR=\"" + (String) WLAN_SSID + "\",\"" + (String) WLAN_PASSWORD + "\"," + (String) WLAN_CHANNEL + ",3,1"); //setting SSID, Password, Channel, Encryption (3=WPA2_PSK), Only one user
+  delay(100);
 }
 
 char parseSerialPercentage()                //Parses the next 4 Characters in Serial Buffer based on the Rules listed at the beginning of this Program
 {
   char percent = 0;
-  if (Serial.read() == '+') {
-    percent += (Serial.read() - '0') * 100;
-    percent += (Serial.read() - '0') * 10;
-    percent += (Serial.read() - '0') * 1;
+  if ( ESP8266 .read() == '+') {
+    percent += ( ESP8266 .read() - '0') * 100;
+    percent += (ESP8266.read() - '0') * 10;
+    percent += (ESP8266.read() - '0') * 1;
   } else {
-    percent -= (Serial.read() - '0') * 100;
-    percent -= (Serial.read() - '0') * 10;
-    percent -= (Serial.read() - '0') * 1;
+    percent -= (ESP8266.read() - '0') * 100;
+    percent -= (ESP8266.read() - '0') * 10;
+    percent -= (ESP8266.read() - '0') * 1;
   }
   return percent;
 }
 
 void send(String string)
 {
-  Serial.println("AT+CIPSENDBUF=0," + String(string.length()));
+  ESP8266.println("AT+CIPSENDBUF=0," + String(string.length()));
   delay(2);//is needed for proper sending
-  Serial.println(string);
+  ESP8266.println(string);
 }
